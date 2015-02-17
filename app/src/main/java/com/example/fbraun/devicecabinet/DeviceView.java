b4df67fb4b697de4746a885e7eb86cbb6d537e7a
Braun,Fee <Fee.Braun@brands4friends.de>
2015-02-17 12:23:07 +0100

    book button onclicklistener

--- app/src/main/java/com/example/fbraun/devicecabinet/DeviceView.java
+++ app/src/main/java/com/example/fbraun/devicecabinet/DeviceView.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,6 +38,14 @@ public class DeviceView extends Activity {
                 personImage.setVisibility(View.GONE);
             }
         }
+
+        Button bookDeviceButton = (Button) findViewById(R.id.button);
+        bookDeviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bookDevice(v);
+            }
+        });
     }
 
     //toDo Button book or return
